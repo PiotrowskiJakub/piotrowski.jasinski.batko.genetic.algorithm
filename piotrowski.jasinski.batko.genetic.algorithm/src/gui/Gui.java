e19e4a44e61c74bb3117dd71f532312653db56e1
Jakub Piotrowski <piotrowski.jakub1993@gmail.com>
2014-04-07 22:32:50 +0200

    Gui zrobione :D

---
+++ piotrowski.jasinski.batko.genetic.algorithm/src/gui/Gui.java
@@ -0,0 +1,213 @@
+package gui;
+
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ * 
+ * @author Jakub Piotrowski
+ * @version 1.0 Class to display graphical user interface.
+ */
+public class Gui extends JFrame
+{
+	private static final long serialVersionUID = 113639547652102992L;
+
+	private JPanel mainPanel;
+	private JLabel label1, label2, label3, label4, label5, label6,
+			lab_pokolenie, lab_numpok, lab_dopasowanie, lab_numdop;
+	private JTextField text1, text2, text3, text4, text5, text6;
+	private JButton load, start, stop;
+	private ImagePanel imagePanel;
+	private MyDrawPanel drawPanel;
+	private File imagePath;
+
+	public static void main(String[] args)
+	{
+		new Gui();
+	}
+
+	public Gui()
+	{
+		super("Create images using a genetic algorithm");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// main Panel
+		mainPanel = new JPanel(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+
+		// 1 row
+		label1 = new JLabel("Image: ");
+		label1.setPreferredSize(new Dimension(200, 50));
+		text1 = new JTextField(10);
+		load = new JButton("Load image");
+		load.addActionListener(new LoadListener());
+		c.gridx = 0;
+		c.gridy = 0;
+		mainPanel.add(label1, c);
+		c.gridx = 1;
+		c.gridy = 0;
+		mainPanel.add(text1, c);
+		c.gridx = 2;
+		c.gridy = 0;
+		mainPanel.add(load, c);
+
+		// 2 row
+		label2 = new JLabel("Size of population: ");
+		label2.setPreferredSize(new Dimension(200, 50));
+		text2 = new JTextField("50", 10);
+		// text2.setPreferredSize(new Dimension(50, 20));
+		c.gridx = 0;
+		c.gridy = 2;
+		mainPanel.add(label2, c);
+		c.gridx = 1;
+		c.gridy = 2;
+		mainPanel.add(text2, c);
+
+		// 3 row
+		label3 = new JLabel("Size of elite: ");
+		label3.setPreferredSize(new Dimension(200, 50));
+		text3 = new JTextField("10", 10);
+		c.gridx = 0;
+		c.gridy = 3;
+		mainPanel.add(label3, c);
+		c.gridx = 1;
+		c.gridy = 3;
+		mainPanel.add(text3, c);
+
+		// 4 row
+		label4 = new JLabel("Number of elements: ");
+		label4.setPreferredSize(new Dimension(200, 50));
+		text4 = new JTextField("200", 10);
+		c.gridx = 0;
+		c.gridy = 4;
+		mainPanel.add(label4, c);
+		c.gridx = 1;
+		c.gridy = 4;
+		mainPanel.add(text4, c);
+
+		// 5 row
+		label5 = new JLabel("The probability of mutation: ");
+		label5.setPreferredSize(new Dimension(200, 50));
+		text5 = new JTextField("0.1", 10);
+		c.gridx = 0;
+		c.gridy = 5;
+		mainPanel.add(label5, c);
+		c.gridx = 1;
+		c.gridy = 5;
+		mainPanel.add(text5, c);
+
+		// 6 row
+		label6 = new JLabel("Size of mutations (1-100): ");
+		label6.setPreferredSize(new Dimension(200, 50));
+		text6 = new JTextField("10.0", 10);
+		c.gridx = 0;
+		c.gridy = 6;
+		mainPanel.add(label6, c);
+		c.gridx = 1;
+		c.gridy = 6;
+		mainPanel.add(text6, c);
+
+		// 7 row
+		start = new JButton("Start");
+		start.addActionListener(new StartListener());
+		stop = new JButton("Stop");
+		c.gridx = 0;
+		c.gridy = 7;
+		mainPanel.add(start, c);
+		c.gridx = 1;
+		c.gridy = 7;
+		mainPanel.add(stop, c);
+
+		// 8 row
+		imagePanel = new ImagePanel();
+		drawPanel = new MyDrawPanel();
+		c.gridx = 0;
+		c.gridy = 8;
+		mainPanel.add(imagePanel, c);
+		c.gridx = 1;
+		c.gridy = 8;
+		mainPanel.add(drawPanel, c);
+
+		// 9 row
+		lab_pokolenie = new JLabel("Generation: ");
+		lab_pokolenie.setPreferredSize(new Dimension(200, 50));
+		lab_numpok = new JLabel("0");
+		c.gridx = 0;
+		c.gridy = 9;
+		mainPanel.add(lab_pokolenie, c);
+		c.gridx = 1;
+		c.gridy = 9;
+		mainPanel.add(lab_numpok, c);
+
+		// 10 row
+		lab_dopasowanie = new JLabel("Fitness: ");
+		lab_dopasowanie.setPreferredSize(new Dimension(200, 50));
+		lab_numdop = new JLabel("0 %");
+		c.gridx = 0;
+		c.gridy = 10;
+		mainPanel.add(lab_dopasowanie, c);
+		c.gridx = 1;
+		c.gridy = 10;
+		mainPanel.add(lab_numdop, c);
+
+		this.getContentPane().add(mainPanel);
+		this.pack();
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		this.setLocation(dim.width / 2 - this.getSize().width / 2, dim.height
+				/ 2 - this.getSize().height / 2);
+		this.setVisible(true);
+	}
+
+	private class StartListener implements ActionListener
+	{
+
+		@Override
+		public void actionPerformed(ActionEvent e)
+		{
+			if (imagePath == null)
+				JOptionPane.showMessageDialog(Gui.this,
+						"Please choose the image.", "No image",
+						JOptionPane.ERROR_MESSAGE);
+			else
+			{
+				imagePanel.drawImage(imagePath);
+				imagePanel.setPreferredSize(new Dimension(
+						imagePanel.getWidth(), imagePanel.getHeight()));
+				
+				drawPanel.setWidth(imagePanel.getWidth());
+				drawPanel.setHeight(imagePanel.getHeight());
+				drawPanel.setPreferredSize(new Dimension(imagePanel.getWidth(), imagePanel.getHeight()));
+				
+				Gui.this.repaint();
+				Gui.this.pack();
+				Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+				Gui.this.setLocation(dim.width / 2 - Gui.this.getSize().width
+						/ 2, dim.height / 2 - Gui.this.getSize().height / 2);
+			}
+		}
+	}
+
+	private class LoadListener implements ActionListener
+	{
+		@Override
+		public void actionPerformed(ActionEvent e)
+		{
+			JFileChooser chooser = new JFileChooser("img/");
+			chooser.showOpenDialog(Gui.this);
+			imagePath = chooser.getSelectedFile();
+			text1.setText(imagePath.getName());
+		}
+	}
+}
---
+++ piotrowski.jasinski.batko.genetic.algorithm/src/gui/ImagePanel.java
@@ -0,0 +1,55 @@
+package gui;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+/**
+ * 
+ * @author Jakub Piotrowski
+ * @version 1.0
+ * Class to display image.
+ */
+public class ImagePanel extends JPanel
+{
+	private static final long serialVersionUID = 3006769532505931833L;
+	private BufferedImage image;
+	private int width, height;
+	
+	public void drawImage(File file)
+	{
+		try
+		{
+			image = ImageIO.read(file);
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+		width = image.getWidth();
+		height = image.getHeight();
+		
+		paintComponent(this.getGraphics());
+	}
+
+	@Override
+	protected void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+		g.drawImage(image, 0, 0, null);
+	}
+
+	public int getWidth()
+	{
+		return width;
+	}
+
+	public int getHeight()
+	{
+		return height;
+	}
+}
